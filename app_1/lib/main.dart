b47235be59ee578416e965156dfde75ec561a459
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2022-12-31 17:00:55 -0300

    instanciacao Task

--- app_1/lib/main.dart
+++ app_1/lib/main.dart
@@ -17,30 +17,13 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: const Text('Tarefas')),
-        body: Container(
-          child: Stack(children: [
-            Container(
-              color: Colors.blue,
-              height: 140,
-            ),
-            Container(
-              color: Colors.white,
-              height: 100,
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                    ),
-                    const Text('Aprendendo Flutter'),
-                    ElevatedButton(
-                        onPressed: () {},
-                        child: const Icon(Icons.arrow_drop_up_sharp))
-                  ]),
-            )
-          ]),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Task(),
+            Task(),
+            Task(),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: (() {}),
@@ -50,11 +33,34 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Stack(children: [
+        Container(
+          color: Colors.blue,
+          height: 140,
+        ),
+        Container(
+          color: Colors.white,
+          height: 100,
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Container(
+              color: Colors.black26,
+              width: 72,
+              height: 100,
+            ),
+            const Text('Aprendendo Flutter'),
+            ElevatedButton(
+                onPressed: () {}, child: const Icon(Icons.arrow_drop_up_sharp))
+          ]),
+        )
+      ]),
+    );
   }
 }
