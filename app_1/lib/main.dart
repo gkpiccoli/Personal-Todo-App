537842c4b6aa7f70dba8463f6dc69dcd63bba5bb
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2022-12-31 17:38:01 -0300

    elipsis

--- app_1/lib/main.dart
+++ app_1/lib/main.dart
@@ -17,12 +17,16 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: const Text('Tarefas')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: const [
             Task('Viagens'),
             Task('Dicas de Hotéis e Bares'),
-            Task('Locais de visitação'),
+            Task(
+                'Locais de visitação quando o dia estiver ensolarado ou você queira se aventurar'),
+            Task('Negócios'),
+            Task('Tendências'),
+            Task('Cultura')
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -39,28 +43,40 @@ class Task extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(children: [
-        Container(
-          color: Colors.blue,
-          height: 140,
-        ),
-        Container(
-          color: Colors.white,
-          height: 100,
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Container(
-              color: Colors.black26,
-              width: 72,
-              height: 100,
-            ),
-            Text(nome),
-            ElevatedButton(
-                onPressed: () {}, child: const Icon(Icons.arrow_drop_up_sharp))
-          ]),
-        )
-      ]),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    width: 72,
+                    height: 100,
+                  ),
+                  Container(
+                    width: 200,
+                    child: Text(
+                      nome,
+                      style: TextStyle(
+                          fontSize: 24, overflow: TextOverflow.ellipsis),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: const Icon(Icons.arrow_drop_up_sharp))
+                ]),
+          )
+        ]),
+      ),
     );
   }
 }
