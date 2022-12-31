612b257ea7a530e31fa7fa540c87c91f245f9240
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2022-12-30 23:12:01 -0300

    stless

--- app_1/lib/main.dart
+++ app_1/lib/main.dart
@@ -49,3 +49,12 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
