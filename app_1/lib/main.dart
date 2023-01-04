4df0a03b0ce4b0d57e069d50e506a1bf61360952
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-04 11:08:18 -0300

    update main.dart

--- app_1/lib/main.dart
+++ app_1/lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
             Tasks(
-              'Andar de Bike',
+              'Correr',
             ),
             Tasks(
               'Ler',
