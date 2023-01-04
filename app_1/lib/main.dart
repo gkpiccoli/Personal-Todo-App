28f53669d8b6643214d9f2b9b0ff18e6127a5928
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-04 12:17:04 -0300

    assets

--- app_1/lib/main.dart
+++ app_1/lib/main.dart
@@ -24,22 +24,21 @@ class MyApp extends StatelessWidget {
           children: const [
             Padding(
               padding: EdgeInsets.only(top: 8),
-              child: Tasks(
-                'Estudar Flutter',
-              ),
+              child: Tasks('Estudar Flutter',
+                  'https://firebasestorage.googleapis.com/v0/b/dashatar-dev.appspot.com/o/dashatars%2FRGFzaGF0YXJfRGV2ZWxvcGVyX05ldXRyYWxfY29sb3JfTl9QV19zaGFkb3c=.png?alt=media'),
             ),
             Tasks(
               'Correr',
+              'https://images.unsplash.com/photo-1573475620830-d9f2e9459ffe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
             ),
-            Tasks(
-              'Ler',
-            ),
-            Tasks(
-              'Meditar',
-            ),
-            Tasks(
-              'Faxinar',
-            )
+            Tasks('Ler',
+                'https://images.unsplash.com/photo-1499257398700-43669759a540?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+            Tasks('Meditar',
+                'https://images.unsplash.com/photo-1604078893234-ff3a1a5d5292?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=661&q=80'),
+            Tasks('Faxinar',
+                'https://images.unsplash.com/photo-1563453392212-326f5e854473?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+            Tasks('Comer',
+                'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1153&q=80')
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -52,7 +51,8 @@ class MyApp extends StatelessWidget {
 
 class Tasks extends StatefulWidget {
   final String nome;
-  const Tasks(this.nome, {Key? key}) : super(key: key);
+  final String foto;
+  const Tasks(this.nome, this.foto, {Key? key}) : super(key: key);
 
   @override
   State<Tasks> createState() => _TasksState();
@@ -92,7 +92,9 @@ class _TasksState extends State<Tasks> {
                         height: 100,
                         color: Colors.black26,
                         child: Image.network(
-                            'https://firebasestorage.googleapis.com/v0/b/dashatar-dev.appspot.com/o/dashatars%2FRGFzaGF0YXJfRGV2ZWxvcGVyX05ldXRyYWxfY29sb3JfTl9QV19zaGFkb3c=.png?alt=media'),
+                          widget.foto,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                       Container(
                         width: 200,
