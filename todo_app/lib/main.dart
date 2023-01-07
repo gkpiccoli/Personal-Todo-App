c5a5dac5350c41c21e42ce63e922e867ac76ebcc
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-07 16:50:54 -0300

    update

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'difficulty.dart';
+import 'initial_screen.dart';
 import 'task.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool opacidade = true;
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -24,58 +25,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Tarefas'),
-          leading: Container(),
-        ),
-        body: AnimatedOpacity(
-          opacity: opacidade ? 1 : 0,
-          duration: const Duration(milliseconds: 500),
-          child: ListView(
-            children: const [
-              Padding(
-                padding: EdgeInsets.only(top: 8),
-                child: Tasks(
-                  'Estudar Flutter',
-                  'https://firebasestorage.googleapis.com/v0/b/dashatar-dev.appspot.com/o/dashatars%2FRGFzaGF0YXJfRGV2ZWxvcGVyX05ldXRyYWxfY29sb3JfTl9QV19zaGFkb3c=.png?alt=media',
-                  2,
-                ),
-              ),
-              Tasks(
-                'Correr',
-                'https://images.unsplash.com/photo-1573475620830-d9f2e9459ffe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-                4,
-              ),
-              Tasks(
-                  'Ler',
-                  'https://images.unsplash.com/photo-1499257398700-43669759a540?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-                  3),
-              Tasks(
-                  'Meditar',
-                  'https://images.unsplash.com/photo-1604078893234-ff3a1a5d5292?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=661&q=80',
-                  5),
-              Tasks(
-                  'Faxinar',
-                  'https://images.unsplash.com/photo-1563453392212-326f5e854473?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-                  4),
-              Tasks(
-                  'Comer',
-                  'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1153&q=80',
-                  0)
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              opacidade = !opacidade;
-            });
-          },
-          child: const Icon(Icons.remove_red_eye),
-        ),
-      ),
+      home: InitialScreen()
     );
   }
 }
-
