616b6d5f7f17de6f73e8286cb08066ecade090d8
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-07 16:10:29 -0300

    refactor

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'difficulty.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -142,7 +144,7 @@ class _TasksState extends State<Tasks> {
                           ),
                         ),
                         Difficulty(
-                          difficultyLevel: 0,
+                          difficultyLevel: widget.dificuldade,
                         )
                       ],
                     ),
@@ -204,47 +206,6 @@ class _TasksState extends State<Tasks> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class Difficulty extends StatelessWidget {
-  final int difficultyLevel;
-  const Difficulty({
-    Key? key,
-    required this.difficultyLevel,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(
-          Icons.star,
-          size: 15,
-          color: (difficultyLevel >= 1) ? Colors.blue : Colors.blue[100],
-        ),
-        Icon(
-          Icons.star,
-          size: 15,
-          color: (difficultyLevel >= 2) ? Colors.blue : Colors.blue[100],
-        ),
-        Icon(
-          Icons.star,
-          size: 15,
-          color: (difficultyLevel >= 3) ? Colors.blue : Colors.blue[100],
-        ),
-        Icon(
-          Icons.star,
-          size: 15,
-          color: (difficultyLevel >= 4) ? Colors.blue : Colors.blue[100],
-        ),
-        Icon(
-          Icons.star,
-          size: 15,
-          color: (difficultyLevel >= 5) ? Colors.blue : Colors.blue[100],
-        ),
-      ],
     );
   }
 }
