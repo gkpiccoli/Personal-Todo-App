2977a1b2a2c13831fe3302a4d408bb12fc754a96
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-09 10:05:32 -0300

    assets

--- todo_app/lib/components/task.dart
+++ todo_app/lib/components/task.dart
@@ -49,7 +49,7 @@ class _TasksState extends State<Tasks> {
                           BoxDecoration(borderRadius: BorderRadius.circular(4)),
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(4),
-                        child: Image.network(
+                        child: Image.asset(
                           widget.foto,
                           fit: BoxFit.cover,
                         ),
--- todo_app/lib/screens/initial_screen.dart
+++ todo_app/lib/screens/initial_screen.dart
@@ -26,33 +26,13 @@ class _InitialScreenState extends State<InitialScreen> {
           children: const [
             Padding(
               padding: EdgeInsets.only(top: 8),
-              child: Tasks(
-                'Estudar Flutter',
-                'https://firebasestorage.googleapis.com/v0/b/dashatar-dev.appspot.com/o/dashatars%2FRGFzaGF0YXJfRGV2ZWxvcGVyX05ldXRyYWxfY29sb3JfTl9QV19zaGFkb3c=.png?alt=media',
-                2,
-              ),
+              child: Tasks('Estudar Flutter', 'assets/images/estudar.png', 2),
             ),
-            Tasks(
-              'Correr',
-              'https://images.unsplash.com/photo-1573475620830-d9f2e9459ffe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-              4,
-            ),
-            Tasks(
-                'Ler',
-                'https://images.unsplash.com/photo-1499257398700-43669759a540?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-                3),
-            Tasks(
-                'Meditar',
-                'https://images.unsplash.com/photo-1604078893234-ff3a1a5d5292?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=661&q=80',
-                5),
-            Tasks(
-                'Faxinar',
-                'https://images.unsplash.com/photo-1563453392212-326f5e854473?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-                4),
-            Tasks(
-                'Comer',
-                'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1153&q=80',
-                1),
+            Tasks('Correr', 'assets/images/correr.jpg', 4),
+            Tasks('Ler', 'assets/images/ler.jpg', 3),
+            Tasks('Meditar', 'assets/images/meditar.jpg', 5),
+            Tasks('Faxinar', 'assets/images/limpar.jpg', 4),
+            Tasks('Comer', 'assets/images/comer.jpg', 1),
             SizedBox(
               height: 80,
             )
